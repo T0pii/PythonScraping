b89bad9e55a23cc92f2449fdf431d428e6066b37
T0pii <lucas.co.pro@gmail.com>
2023-02-22 15:00:40 +0100

    update getInfoByPage

--- scrap.py
+++ scrap.py
@@ -10,13 +10,20 @@ def getEndpoints(soup):
   div = soup.find('div', {'class': 'conteneur_encadres_residences'})
   divs = soup.findAll('div', {'class': 'encadre_residence_titre_nom'})
   for div in divs:
-    a = div.find('a') 
+    a = div.find('a')
     links.append(a["href"])
   return links
 
 def getInfoByPage(soup):
-  infosTriees = [soup]
-  return infosTriees
+  domaine = soup.find('div', {'class' : 'domaine'}).getText().replace("Domaine ",'')
+  altitude = soup.find('div', {'class' : 'pistes'}).getText().replace("Altitude ", '')
+  avis = soup.find('span', {'class' : 'note'}).getText().replace('/10','')
+  infos = {
+    "domaine" : domaine,
+    "altitude" : altitude,
+    "avis" : avis
+  }
+  return infos
 
 def getSoup(url, process):
   response = requests.get(url)
@@ -25,34 +32,27 @@ def getSoup(url, process):
     return process(soup)
   return []
 
-def fileReader(file):
-  result = []
-  with open(file, 'r', encoding="UTF8", newline="") as f:
-    reader = csv.DictReader(f)
-    for line in reader:
-      result.append(line)
-  return result
-
 def fileWriter(file, fieldnames, data):
   result = []
   with open(file, 'w', encoding="UTF8", newline="") as f:
     writer = csv.DictWriter(f, fieldnames=fieldnames)
     writer.writeheader()
-    writer.writerows(data)
-  return fileReader(file)
+    for d in data:
+      writer.writerow(d)
 
-data = fileReader("links.csv")
-
-fields =  ['test']
-fileWriter('infos.csv', fields, data)
-exit()
+"""soup = BeautifulSoup(requests.get("https://www.ski-planet.com/fr/location-ski/residence-appart-vacances-pyrenees-2000_pyrenees-2000.html").text, 'html.parser') 
+print(soup.findAll('div',{'class': 'bloc-bandeau'}))
+"""
 
 endpoints = getSoup(baseUrl + uri, getEndpoints)
 
-print(endpoints)
+endpointsDict = []
+for endpoint in endpointsDict:
+  endpointsDict.append({"link" : endpoint})
 
-result = []
+allResults = []
 for endpoint in endpoints:
-  result.extend(getSoup(endpoint,getInfoByPage))
+  allResults.append(getSoup(endpoint, getInfoByPage))
 
-print(result)
+print(allResults)
+fileWriter('links.csv',['link'],endpointsDict)
